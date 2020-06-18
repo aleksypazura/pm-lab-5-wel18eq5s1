7043fed6f12a730e8ba426cfe118b0b8ec3dcb35
AleksyPazura <aleksy.pazura@student.wat.edu.pl>
2020-06-18 09:41:29 +0200

    Wykonano zadanie 3.3.2 - tekst pływający

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -3,14 +3,37 @@
 
 //Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB%, DB6, DB7
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+char tablica[]="Aleksy";
+int tabLen = 0;
+
+void lcdCLear(void);
 
 void setup() {  
-    lcd.begin (16, 2);
-    lcd.print("Pazura");
+    lcd.begin(16, 2);
+    lcd.print("Aleksy Pazura");
+    tabLen = sizeof(tablica);
+    delay(2000);
 }
 
-void loop() {  
+void loop() { 
+    for(int i=0; i<(18 - tabLen); i++){ 
+        lcdClear();
+        lcd.setCursor(i, 0);
+        lcd.print(tablica);
+        delay(1000);
+    }
+
+    for(int i=(17 - tabLen); i>=0; i--){
+        lcdClear();
+        lcd.setCursor(i, 1);
+        lcd.print(tablica);
+        delay(1000);
+    }
+}
+
+void lcdClear(void){
+    lcd.setCursor(0, 0);
+    lcd.print("                ");
     lcd.setCursor(0, 1);
-    lcd.print(millis()/1000);
-    delay(500);
+    lcd.print("                ");
 }
