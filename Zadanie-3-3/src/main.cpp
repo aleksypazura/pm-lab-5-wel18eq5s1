c2c58288b32921db88b362885f8933f8949c3f74
AleksyPazura <aleksy.pazura@student.wat.edu.pl>
2020-06-18 09:23:23 +0200

    Wykonano zadanie 3.3.1 - tekst statyczny

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,7 +1,16 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+//Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB%, DB6, DB7
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {  
+    lcd.begin (16, 2);
+    lcd.print("Pazura");
 }
 
 void loop() {  
+    lcd.setCursor(0, 1);
+    lcd.print(millis()/1000);
+    delay(500);
 }
